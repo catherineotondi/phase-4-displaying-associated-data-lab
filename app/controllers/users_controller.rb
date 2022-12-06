5eee9bd400e616cb133798bf1c07586d4f8b40c8
catherineotondi <catherineotondi@gmail.com>
2022-12-06 15:41:36 +0300

    commit tree 8b1610a633565754cd6214c3ccf89803095df93a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def show
+        user = User.find_by(id: params[:id])
+        render json:user, include: :items
+    end
+    end
